8ba620d172bd817a85e645de18b815f32c7e0865
KICKR <ki7ick@gmail.com>
2024-09-04 14:42:23 +0800

    feat: 第一种方式调试jsx

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -64,4 +64,33 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
   return ReactElement(type, key, ref, props);
 };
 
-export const jsxDev = jsx;
+export const jsxDEV = (type: ElementType, config: any) => {
+  let key: Key = null;
+  const props: Props = {};
+  let ref: Ref = null;
+
+  for (const prop in config) {
+    const val = config[prop];
+
+    if (prop === "key") {
+      if (val !== undefined) {
+        key = "" + val;
+      }
+      continue;
+    }
+
+    if (prop === "ref") {
+      if (val !== undefined) {
+        ref = val;
+      }
+      continue;
+    }
+
+    if ({}.hasOwnProperty.call(config, prop)) {
+      // 不处理原型链上的属性
+      props[prop] = val;
+    }
+  }
+
+  return ReactElement(type, key, ref, props);
+};
